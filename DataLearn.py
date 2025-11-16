b3b689ffa79201be52f160c8538ebc02856e00e1
Kavin-Ramesh <kavinramesh07@gmail.com>
2025-11-15 18:52:08 -0800

    DataCleaning Done

--- DataLearn.py
+++ DataLearn.py
@@ -1,15 +1,64 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords #stopwords for english
+from nltk.tokenize import word_tokenize #word tokenizer
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+stop_words_english = stopwords.words('english')
 
-fake_news = pd.read_csv('Fake.csv')
-real_news = pd.read_csv('True.csv')
+fake_news_df = pd.read_csv('Fake.csv')
+real_news_df = pd.read_csv('True.csv')
 
-print(fake_news.head())
-print(real_news.head())
-print(fake_news.columns)
-print(real_news.columns)
+#Columns in each dataset ['title', 'text', 'subject', 'date']
+
+#Step 1: Cleaning the Fake News Dataset
+
+fn_text_columns = ['title', 'text', 'subject']
+
+for col in fn_text_columns:
+    fake_news_df[col] = (
+        fake_news_df[col]
+        .astype(str)
+        .str.lower()
+        .str.replace("[^a-zA-Z ]", " ", regex=True)  # removes apostrophes too
+    )
+
+for col in fn_text_columns:
+    fake_news_df[col] = (
+        fake_news_df[col]
+        .apply(word_tokenize)
+        .apply(lambda x: [word for word in x if word not in stop_words_english])
+        .apply(lambda x: ' '.join(x))
+    )
+
+fake_news_df = fake_news_df[fake_news_df['text'].str.len() > 0]
+
+# Stemming and Lemmatization was intentionally not used as our datasets did not come close to being large enough to require it.
+
+#Step 1a: Cleaning in the real news Dataset
+
+rn_text_columns = ['title', 'text', 'subject']
+
+for col in rn_text_columns:
+    real_news_df[col] = (
+        real_news_df[col]
+        .astype(str)
+        .str.lower()
+        .str.replace("[^a-zA-Z ]", " ", regex=True)  # removes apostrophes too
+    )
+
+for col in rn_text_columns:
+    real_news_df[col] = (
+        real_news_df[col]
+        .apply(word_tokenize)
+        .apply(lambda x: [word for word in x if word not in stop_words_english])
+        .apply(lambda x: ' '.join(x))
+    )
+
+real_news_df = real_news_df[real_news_df['text'].str.len() > 0]
+
+#Step 2: Finding Patterns in Words of the Fake News Dataset
